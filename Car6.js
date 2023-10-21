d36e46aee6068580279c39e4e59c23c1fb0fe268
linhtql <tqlinh.na@gmail.com>
2023-10-21 21:32:45 +0700

    React-Class

---
+++ Car6.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Car6 extends React.Component {
+    render() {
+        return (
+            <h2>Hi, I am a Car!</h2>
+        )
+    }
+}
+
+export default Car6;
